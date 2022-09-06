556b525ae46831f872f4e11707197995062da228
attika19195 <attika19195@gmail.com>
2022-09-06 20:50:06 +0200

    oszlopok helyiértékének meghatározása

--- src/holakartya/HolAKartya.java
+++ src/holakartya/HolAKartya.java
@@ -1,26 +1,24 @@
 package holakartya;
+import java.util.Scanner;
 
 public class HolAKartya {
 
     public static void main(String[] args) {
         
+        
         String [] szinek = {"P", "T", "Z", "M"};
         String [] ertekek = {"Asz", "Kir", "Fel", "X", "IX", "VIII"};
         String [] pakli = new String[21];
-        
-        
-        
+            
         /*pakli letrehozasa*/
         int kartya = 0;
-        for (int szin = 0; szin < szinek.length; szin++) {
-            for (int ertek = 0; ertek < ertekek.length; ertek++) {
-                if (!("M".equals(szinek[szin]) && ("X".equals(ertekek[ertek]) || "IX".equals(ertekek[ertek]) || "VIII".equals(ertekek[ertek])))) {
-                    pakli[kartya] = szinek[szin]+"_"+ertekek[ertek];
+        for (String szinek1 : szinek) {
+            for (String ertekek1 : ertekek) {
+                if (!("M".equals(szinek1) && ("X".equals(ertekek1) || "IX".equals(ertekek1) || "VIII".equals(ertekek1)))) {
+                    pakli[kartya] = szinek1 + "_" + ertekek1;
                     kartya++;
                 }
-                
             }
-            
         }
         
         
@@ -34,6 +32,26 @@ public class HolAKartya {
             
         }
         System.out.println("");
+        System.out.println();
+        
+        
+        
+        int oszlop1[] = {0, 3, 6, 9, 12, 15, 18};
+        int oszlop2[] = {1, 4, 7, 10, 13, 16, 19};
+        int oszlop3[] = {2, 5, 8, 11, 14, 19, 21};
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Melyik oszlopban van(1/2/3)?");
+        int tipp = input.nextInt();
+        System.out.println(tipp);
+        
+        
+        
+        
+        
     }   
+        
+    
+    
     
 }
